e3120c73a669fdcde236674224f58fd55dc37e22
unknown <ssalaudeen10@gmail.com>
2022-06-11 12:39:36 +0100

    Ninja code version one

---
+++ android/app/src/main/kotlin/com/solution/ninja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.solution.ninja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: NinjaHomePage(),
+    ),
+  );
+}
+
+class NinjaHomePage extends StatelessWidget {
+  const NinjaHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
