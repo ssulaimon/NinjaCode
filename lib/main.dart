81900f70dc484daa332133f91ee2e4f36f72635b
unknown <ssalaudeen10@gmail.com>
2022-06-11 13:32:09 +0100

    more code added

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,99 @@ class NinjaHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage(
+                "assets/ninja.png",
+              ),
+              radius: 40.0,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Divider(
+            thickness: 2.0,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Code Name:",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20.0, letterSpacing: 2.0),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          const Text(
+            "Ninja killer code name",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20.0, letterSpacing: 2.0),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Battle fought:",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20.0, letterSpacing: 2.0),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "3",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20.0, letterSpacing: 2.0),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Level",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20.0, letterSpacing: 2.0),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            children: const [
+              Icon(
+                Icons.star,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.white,
+              )
+            ],
+          )
+        ],
+      ),
+      backgroundColor: Colors.black,
+    );
   }
 }
